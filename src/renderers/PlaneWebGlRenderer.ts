eb7d6dae81d12f1b08a9d5f507586ea7a8cdf884
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2022-11-18 22:05:58 +0100

    Some Optimalizations

--- src/renderers/PlaneWebGlRenderer.ts
+++ src/renderers/PlaneWebGlRenderer.ts
@@ -7,78 +7,27 @@ import * as twgl from "twgl.js"
 // https://twgljs.org/docs/module-twgl.html
 // https://stackoverflow.com/questions/37927359/how-to-draw-2d-image-with-twgl-webgl-helper-library
 export class PlaneWebGlRenderer implements IRenderer {
-    private imageCache: ImageCache;
-    private texture: WebGLTexture | undefined;
     private image: HTMLImageElement | undefined;
-    private programInfo: twgl.ProgramInfo | undefined;
-    private gl: WebGLRenderingContext;
-
-    /** a unit quad */
-    private bufferInfo: twgl.BufferInfo;
+    private readonly  programInfo: twgl.ProgramInfo | undefined;
+    private readonly  gl: WebGLRenderingContext;
+      /** a unit quad */
+    private readonly bufferInfo: twgl.BufferInfo;
+    private readonly uniforms: {
+        texture: WebGLTexture | undefined;
+        textureMatrix: twgl.m4.Mat4 | undefined;
+        matrix: twgl.m4.Mat4 | undefined;
+    };
 
     constructor() {
-        this.imageCache = new ImageCache();
-
         // try to create a WebGL canvas (will fail if WebGL isn't supported)
         const canvas = document.createElement("canvas");
         const gl = canvas.getContext("webgl") || undefined;
         if (!gl) throw new Error("WebGL not supported");
         this.gl = gl;
 
-        this.bufferInfo = twgl.primitives.createXYQuadBufferInfo(gl);;
+        this.bufferInfo = twgl.primitives.createXYQuadBufferInfo(gl);
 
-    }
-
-    /**
-     * Load the image and set the texture and image member variable
-     */
-    loadAsync(
-        config: IPlaneConfig2,
-    ) {
-        if (this.image) return; // TODO, compare if same image is loaded (add loadedUrl member variable to compare?)
-
-        return new Promise<void>((resolve, reject) => {
-            const { gl } = this;
-            this.image = undefined;
-            this.texture = twgl.createTexture(gl, {
-                // src: config.image.imageUrl,
-                src: "https://farm6.staticflickr.com/5695/21506311038_9557089086_m_d.jpg",
-                crossOrigin: '', // not needed if image on same origin
-            }, (err, tex, img) => {
-                // wait for the image to load because we need to know it's size
-                if (err) {
-                    this.image = undefined;
-                    this.texture = undefined;
-                    reject(err);
-                } else {
-                    this.image = img as HTMLImageElement;
-                    this.texture = tex;
-                    resolve();
-                }
-            });
-        });
-    }
-
-    render(
-        targetContext: CanvasRenderingContext2D,
-        config: IPlaneConfig2
-    ): void {
-        const { gl, texture, image, bufferInfo } = this;
-        if (!texture || !image) throw new Error("Only call render after a succesful loadAsync");
-        const m4 = twgl.m4;
-        const webGlCanvas = gl.canvas as HTMLCanvasElement;
-        const { width: targetWidth, height: targetHeight } = targetContext.canvas;
-
-        // Set size
-        webGlCanvas.width = targetWidth;
-        webGlCanvas.height = targetHeight;
-        gl.viewport(0, 0, targetWidth, targetHeight);
-
-        // Set/Clear background
-        // gl.clearColor(1.0, 1.0, 1.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        const vs = `
+        const vertexShader = `
             // we will always pass a 0 to 1 unit quad
             // and then use matrices to manipulate it
             attribute vec4 position;   
@@ -94,7 +43,7 @@ export class PlaneWebGlRenderer implements IRenderer {
             }
         `;
 
-        const fs = `
+        const fragmentShader = `
             precision mediump float;
 
             varying vec2 texcoord;
@@ -109,28 +58,79 @@ export class PlaneWebGlRenderer implements IRenderer {
             }
         `;
 
-        const programInfo = twgl.createProgramInfo(gl, [vs, fs], (err) => {
+        this.programInfo = twgl.createProgramInfo(gl, [vertexShader, fragmentShader], (err) => {
             console.error(err); // TODO: Error handling
         });
 
-        // Convert from pixels to clip space
-        const matrix = m4.identity();
-        m4.ortho(0, targetWidth, targetHeight, 0, -1, 1, matrix);
-        
-        m4.translate(matrix, [0, 0, 0], matrix);
-        m4.scale(matrix, [targetWidth, targetHeight, 1], matrix);
-
-        const uniforms = {
-            texture,
-            textureMatrix: m4.identity(),
-            matrix,
+        this.uniforms = {
+            texture: undefined,
+            textureMatrix: twgl.m4.identity(),
+            matrix: undefined,
         };
 
+        gl.useProgram(this.programInfo.program);
+        twgl.setBuffersAndAttributes(gl, this.programInfo, this.bufferInfo);
+    }
+
+    /**
+     * Load the image and set the texture and image member variable
+     */
+    loadAsync(
+        config: IPlaneConfig2,
+    ) {
+        if (this.image) return; // TODO, compare if same image is loaded (add loadedUrl member variable to compare?)
+
+        return new Promise<void>((resolve, reject) => {
+            const { gl } = this;
+            this.image = undefined;
+            this.uniforms.texture = twgl.createTexture(gl, {
+                // src: config.image.imageUrl,
+                src: "https://farm6.staticflickr.com/5695/21506311038_9557089086_m_d.jpg",
+                crossOrigin: '', // not needed if image on same origin
+            }, (err, tex, img) => {
+                // wait for the image to load because we need to know it's size
+                if (err) {
+                    this.image = undefined;
+                    this.uniforms.texture = undefined;
+                    reject(err);
+                } else {
+                    this.image = img as HTMLImageElement;
+                    this.uniforms.texture = tex;
+                    resolve();
+                }
+            });
+        });
+    }
+
+    render(
+        targetContext: CanvasRenderingContext2D,
+        config: IPlaneConfig2
+    ): void {
+        const { gl, uniforms, image, bufferInfo, programInfo } = this;
+        if (!uniforms.texture || !image || !programInfo) throw new Error("Only call render after a succesful loadAsync");
+        const m4 = twgl.m4;
+        const webGlCanvas = gl.canvas as HTMLCanvasElement;
+        const { width: targetWidth, height: targetHeight } = targetContext.canvas;
+
+        // Set size
+        webGlCanvas.width = targetWidth;
+        webGlCanvas.height = targetHeight;
+        gl.viewport(0, 0, targetWidth, targetHeight);
+
+        // Set/Clear background
+        // gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Calculate matrix
+        const matrix = m4.identity();
+        m4.ortho(0, targetWidth, targetHeight, 0, -1, 1, matrix); // Convert from pixels to clip space
+        m4.translate(matrix, [0, 0, 0], matrix);
+        m4.scale(matrix, [targetWidth, targetHeight, 1], matrix);
+        this.uniforms.matrix = matrix;
+
         // Render
-        gl.useProgram(programInfo.program);
-        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-        twgl.setUniforms(programInfo, uniforms);
-        twgl.drawBufferInfo(gl, bufferInfo);
+        twgl.setUniforms(programInfo, this.uniforms);
+        twgl.drawBufferInfo(gl, this.bufferInfo);
 
         // Draw WebGl canvas on destination canvas
         targetContext.drawImage(
@@ -141,19 +141,16 @@ export class PlaneWebGlRenderer implements IRenderer {
     }
 
     dispose(): void {
-        if (this.texture) {
-            this.gl.deleteTexture(this.texture);
-            this.texture = undefined;
+        if (this.uniforms.texture) {
+            this.gl.deleteTexture(this.uniforms.texture);
         }
 
         if (this.bufferInfo) {
             this.gl.deleteBuffer(this.bufferInfo);
-            this.bufferInfo = undefined!;
         }
 
         if (this.programInfo) {
             this.gl.deleteProgram(this.programInfo);
-            this.programInfo = undefined;
         }
     }
 }
