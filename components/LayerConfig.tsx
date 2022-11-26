4c5556a9465c692ce779637a429d1c3035470340
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2022-11-26 22:07:11 +0100

    Move Layer Components to its own file

---
+++ components/LayerConfig.tsx
@@ -0,0 +1,69 @@
+import { ILayerConfig } from "../src/IPackshot";
+
+
+
+export function LayerConfig({
+    config, onChange
+}: {
+    config: ILayerConfig;
+    onChange: (newConfig: ILayerConfig) => void;
+}) {
+    let composition: GlobalCompositeOperation | "disabled" | "normal";
+    if (config.isDisabled)
+        composition = "disabled";
+    else if (!config.composition)
+        composition = "normal";
+    else
+        composition = config.composition;
+
+    return (
+        <table style={{
+            backgroundColor: "#ccc",
+            boxShadow: "0 5px 8px rgba(0, 0, 0, 0.2) inset",
+            borderBottom: "1px solid #666",
+            marginBottom: 5,
+            padding: 5,
+
+            /** HACK */
+            width: "calc(100% + 10px)",
+            margin: -5,
+        }}>
+            <tbody>
+                <tr>
+                    <td>Composition:</td>
+                    <td>
+                        <select
+                            value={composition}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                if (value === "disable") {
+                                    onChange({
+                                        ...config,
+                                        isDisabled: true,
+                                    });
+                                } else if (value === "normal") {
+                                    onChange({
+                                        ...config,
+                                        isDisabled: false,
+                                        composition: undefined,
+                                    });
+                                } else {
+                                    onChange({
+                                        ...config,
+                                        isDisabled: false,
+                                        composition: value as GlobalCompositeOperation,
+                                    });
+                                }
+                            }}
+                            style={{ minWidth: 150 }}
+                        >
+                            <option value="disable">Disable</option>
+                            <option value="normal">Normal</option>
+                            <option value="multiply">Multiply</option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
---
+++ components/LayerRenderTreeConfig.tsx
@@ -0,0 +1,40 @@
+import { usePackshotActions } from "../src/packshot";
+import { ConfigComponent, getConfigComponent } from "./config/factory";
+import { ILayer } from "../src/IPackshot";
+import { flattenTree, replaceTreeNode } from "../src/Tree";
+
+
+export function LayerRenderTreeConfig({
+    layer, layerIndex,
+}: {
+    layer: ILayer;
+    layerIndex: number;
+}) {
+    const { updateLayerRenderTree } = usePackshotActions();
+
+    const renderers = flattenTree(layer.renderTree);
+
+    const RendererConfigComponents = renderers
+        .map(r => getConfigComponent(r.type))
+        .filter(c => !!c) as ConfigComponent<any>[]; // Remove empty onces
+
+    return (
+        <>
+            {RendererConfigComponents.map((ConfigComponent, rendererIndex) => (
+                <ConfigComponent
+                    key={rendererIndex}
+                    config={renderers[rendererIndex].config}
+                    onChange={(newConfig) => {
+                        const prevRendererNode = renderers[rendererIndex]; // Item to replace
+                        const newRendererNode = {
+                            ...prevRendererNode,
+                            config: newConfig,
+                        };
+
+                        const newRenderTree = replaceTreeNode(layer.renderTree, prevRendererNode, newRendererNode);
+                        updateLayerRenderTree(layerIndex, newRenderTree);
+                    }} />
+            ))}
+        </>
+    );
+}
